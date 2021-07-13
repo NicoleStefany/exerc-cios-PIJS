bac4096534af2981eca4061ebec303635a4755e8
NicoleStefany <85133137+NicoleStefany@users.noreply.github.com>
2021-07-13 13:04:09 -0300

    Add files via upload

---
+++ microondas.js
@@ -0,0 +1,48 @@
+function menu(comidas, tempoPreparo) 
+{
+    let prato;
+
+    switch (comidas) 
+    {
+        case "pipoca":
+            prato=10;
+            microondas(prato,tempoPreparo)
+            break;
+        case "macarrão":
+            prato=8;
+            microondas(prato,tempoPreparo)
+            break;
+        case "carne":
+            prato=15;
+            microondas(prato,tempoPreparo)
+            break;
+        case "feijão":
+            prato=12;
+            microondas(prato,tempoPreparo)
+            break;
+        case "brigadeiro":
+            prato=8;
+            microondas(prato,tempoPreparo)
+            break;
+        default:
+            console.log("Prato inexistente")
+    }
+}
+
+function microondas(prato, tempoPreparo) {
+    if(tempoPreparo >=3 * prato){
+        console.log("KABUUUM!!")
+    } else if(tempoPreparo >=2 * prato){
+        console.log("A comida queimou!")
+    }
+      else if(tempoPreparo < prato){
+            console.log("Tempo insuficiente")
+    } else {
+        console.log("Prato pronto, bom apetite!!")
+    }
+}
+
+
+menu('pipoca',10)
+menu('macarrão',25)
+menu('brigadeiro',20)
